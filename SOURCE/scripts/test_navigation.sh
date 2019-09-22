590b1bdc1c09cb97ba833ae592aa719b3255b8e2
kurbakov <kurbakov.d@gmail.com>
2019-09-22 14:29:11 +0000

    tested that everything works

--- SOURCE/scripts/test_navigation.sh
+++ SOURCE/scripts/test_navigation.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-## STILL NEED TO CHECK IF WORKS!!!!!
-
 ## run turtlebot launch file
 xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/RND_05_project/SOURCE/map/MyWorld.world" &
 sleep 10
--- catkin/build/catkin_generated/order_packages.py
+++ catkin/build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = "/home/workspace/RND_05_project/catkin/src"
 whitelisted_packages = "".split(';') if "" != "" else []
 blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/workspace/RND_05_project/catkin/devel;/opt/ros/kinetic".split(';') if "/home/workspace/RND_05_project/catkin/devel;/opt/ros/kinetic" != "" else []
+underlay_workspaces = "/opt/ros/kinetic".split(';') if "/opt/ros/kinetic" != "" else []
