cd968c5637a940fb2772eb526c672ab2d9c07547
kurbakov <kurbakov.d@gmail.com>
2019-09-22 13:08:15 +0000

    fix sleep time

--- SOURCE/scripts/test_slam.sh
+++ SOURCE/scripts/test_slam.sh
@@ -2,15 +2,15 @@
 
 ## run turtlebot launch file
 xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/RND_05_project/SOURCE/map/MyWorld.world" &
-sleep 5
+sleep 10
 
 ## run slam_gmapping
 xterm -e "roslaunch turtlebot_gazebo gmapping_demo.launch" &
-sleep 5
+sleep 3
 
 ## run rviz
 xterm -e "roslaunch turtlebot_rviz_launchers view_navigation.launch" &
-sleep 5
+sleep 3
 
 ## mrun teleop
 xterm -e "roslaunch turtlebot_teleop keyboard_teleop.launch"
